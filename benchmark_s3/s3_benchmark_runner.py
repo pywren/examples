f1bfe7c8c568c52481a430b7c994b926774baa29
Eric Jonas <jonas@ericjonas.com>
2017-02-28 21:06:50 -0800

    cleaned up benchmark

--- benchmark_s3/s3_benchmark_runner.py
+++ benchmark_s3/s3_benchmark_runner.py
@@ -15,7 +15,7 @@ import s3_benchmark
 
 def ruffus_params():
     for workers in [1, 10, 30, 100, 300, 600, 1000, 2000, 2800]:
-        for seed in range(6):
+        for seed in range(1):
             for mb_per_file in [1000]: # 10, 100, 1000]:
         
                 prefix = "s3_microbench.{}.{}.{}".format(workers, seed, 
@@ -30,6 +30,7 @@ AWS_REGION='us-west-2'
 
 def run_exp(infile, outfile, workers, seed, mb_per_file):
     # write simply writes that number of keys
+    print("running {} {} {} {}".format(outfile, workers, seed, mb_per_file))
     t1 = time.time()
     write_results = s3_benchmark.write(BUCKET_NAME, mb_per_file, workers, 
                                       key_prefix="", region=AWS_REGION)
@@ -49,7 +50,7 @@ def run_exp(infile, outfile, workers, seed, mb_per_file):
                  'mb_per_file' : mb_per_file}, 
                 open(outfile, 'w'), -1)
 
-    print "workers=", workers, "seed=", seed, "mb_per_file=", mb_per_file, "runtime:{:3.0f}".format(t3-t1)
+    print("workers=", workers, "seed=", seed, "mb_per_file=", mb_per_file, "runtime:{:3.0f}".format(t3-t1))
                  
 if __name__ == "__main__":
     pipeline_run([run_exp])
